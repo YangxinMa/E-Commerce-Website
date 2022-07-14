fa07a56e895120be28200a54d99f350a17d000a3
YangxinMa <wghmyx@gmail.com>
2022-07-14 14:10:06 -0700

    Section 17: Shopping Cart Status Component

---
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/components/cart-status/cart-status.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { data } from 'autoprefixer';
+import { CartService } from 'src/app/services/cart.service';
+
+@Component({
+  selector: 'app-cart-status',
+  templateUrl: './cart-status.component.html',
+  styleUrls: ['./cart-status.component.css']
+})
+export class CartStatusComponent implements OnInit {
+
+  totalPrice: number = 0.00;
+  totalQuantity: number = 0;
+
+  constructor(private cartSevice: CartService) { }
+
+  ngOnInit(): void {
+    this.updateCartStatus();
+  }
+
+  updateCartStatus() {
+
+    // subscribe to the cart totalPrice
+    this.cartSevice.totalPrice.subscribe(
+      data => this.totalPrice = data
+    );
+
+    // subscribe to the cart totalQuantity
+    this.cartSevice.totalQuantity.subscribe(
+      data => this.totalQuantity = data
+    );
+  }
+
+}
--- ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -11,12 +13,13 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductDetailsComponent implements OnInit {
 
   product: Product = new Product();
-  
+
   constructor(private productService: ProductService,
+              private cartService: CartService,
               private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(() =>{
+    this.route.paramMap.subscribe(() => {
       this.handleProductDetails();
     }
     )
@@ -32,6 +35,14 @@ export class ProductDetailsComponent implements OnInit {
       }
     )
 
+  }
+
+  addToCart() {
+    
+    console.log(`Adding to cart: ${this.product.name}, ${this.product.unitPrice}`);
+    const theCartItem = new CartItem(this.product);
+    this.cartService.addToCart(theCartItem);
+    
   }
 
 }
