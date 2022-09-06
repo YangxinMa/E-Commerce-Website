6a2dfedad1dc5658e27fff46023785e54c24761e
wghmyx <wghmyx@gmail.com>
2022-09-06 14:07:02 -0700

    Section 25: Login/Logout

---
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId: '0oa24gnw5uOESssWW697',
+        issuer: 'https://trial-3087077.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
